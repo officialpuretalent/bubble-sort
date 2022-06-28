c7b7c2e9dfd940206637bf29a518c648558aa9b7
officialpuretalent <siphumelelotqwabe@gmail.com>
2022-06-28 23:08:10 +0200

    Sorting methods & swap method implemented

--- src/main/java/com/devhouseagency/bubble/BubbleSort.java
+++ src/main/java/com/devhouseagency/bubble/BubbleSort.java
@@ -1,2 +1,21 @@
-package com.devhouseagency.bubble;public class BubbleSort {
+package com.devhouseagency.bubble;
+
+public class BubbleSort {
+    public static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int y = 0; y < array.length - 1; y++) {
+                if (array[y] > array[y + 1]) {
+                    swap(array, y, y + 1);
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
 }
