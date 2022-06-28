dd0508c31c31541ed7c45ca512360b8e80de42dd
officialpuretalent <siphumelelotqwabe@gmail.com>
2022-06-28 23:37:06 +0200

    Doc added in BubbleSortTest class

--- src/test/java/com/devhouseagency/bubble/BubbleSortTest.java
+++ src/test/java/com/devhouseagency/bubble/BubbleSortTest.java
@@ -8,12 +8,17 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class BubbleSortTest {
     @Test
     void canSortArrayWithTwoElements() {
+        // Given that I pass an unordered array in sort method
+        // when I get the first element in the list I must
+        // get 4
         assertEquals(4, BubbleSort.sort(new int[] {5, 4})[0]);
         assertEquals(5, BubbleSort.sort(new int[] {5, 4})[1]);
     }
 
     @Test
     void canReturnOneElement() {
+        // Given that I pass an array with one element I should
+        // get the element as the returned value.
         assertNotEquals(21, BubbleSort.sort(new int[] {10})[0]);
         assertEquals(10, BubbleSort.sort(new int[] {10})[0]);
     }
